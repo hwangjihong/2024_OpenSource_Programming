792e13cb67413ad4a760438f81ae494db2344757
hwangjihong <wlghd6737@gmail.com>
2024-10-04 11:24:48 +0900

    v0.7 Zero Value, Comparsion4

--- week06/main.go
+++ week06/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	i := 13
 	var f float64 = 12.9
-	c1 := 'A'
-	c2 := '김'
+	c1 := 'A' // ASCII
+	c2 := '김' // UNI
 
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	// fmt.Printf("%d * %f = %f\n", i, f, i*f) invalid operation: i * f (mismatched types int and float64)
@@ -17,4 +17,15 @@ func main() {
 	// fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
 	fmt.Println(c1, c2)
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+
+	var f1 float64
+	var i1 int
+	var b bool
+	var s string
+
+	fmt.Println(f1, b, s, i1)
+	fmt.Printf("%f %t %s %d\n", f1, b, s, i1) // zero value
+	f1 = 2.7
+	i1 = 3
+	fmt.Print(f1 > float64(i1), "\n") // comparsion (true/false)
 }
