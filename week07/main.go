901626963f6c8f8b334de505a61f3192b82cdd8c
hwangjihong <wlghd6737@gmail.com>
2024-10-11 10:28:44 +0900

    v1.0 Time

--- week07/main.go
+++ week07/main.go
@@ -10,4 +10,7 @@ func main() {
 	var month int = int(now.Month())
 
 	fmt.Println(month)
+	fmt.Printf("오늘은 %d년 %d월 %d일입니다.\n", now.Year(), now.Month(), now.Day())
+	fmt.Printf("지금은 %d시 %d분 %d초입니다.\n", now.Hour(), now.Minute(), now.Second())
+
 }
